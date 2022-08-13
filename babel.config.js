9d032b585065ef917f6988bf1428e32f46a75e34
LiuChang <lcc1875@163.com>
2022-08-14 00:35:44 +0800

    almost

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
+// 这是项目发布阶段要用到的babel插件
+// 项目开发调试阶段 --mode 为 development  项目发布阶段的 --mode为 production
+const prodPlugins = []
+// 判断项目处于哪个阶段
+if(process.env.NODE_ENV === 'production'){
+  prodPlugins.push('transform-remove-console')
+}
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
@@ -9,6 +16,8 @@ module.exports = {
         "libraryName":"element-ui",
         "styleLibraryName":"theme-chalk"
       }
-    ]
+    ],
+    // 把prodPlugins里的配置运用...运算符展开
+    ...prodPlugins
   ]
 }
---
+++ src/main-dev.js
@@ -0,0 +1,53 @@
+import Vue from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+import router from '@/router/index'
+import './plugins/element'
+import 'font-awesome/css/font-awesome.css'
+import TreeTable from 'vue-table-with-tree-grid'
+
+// 导入 NProgress包对应的JS和CSS
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 在request拦截器中展示进度条 NProgress.start()
+NProgress.start()
+// 设置请求拦截器，添加token令牌，保证拥有获取数据的权限
+// axios请求拦截 为每次API的请求挂载一个包含token的authorization字段的
+axios.interceptors.request.use(config=>{
+  // 为请求头添加token的authorization字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须return config
+  return config
+})
+// 在response拦截器中隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config=>{
+  NProgress.done()
+  return config
+})
+
+Vue.prototype.$http = axios
+
+Vue.config.productionTip = false
+
+// 注册全局的过滤器，第一个参数为过滤器的名称，第二个参数为过滤器的处理函数
+Vue.filter('dateFormat',function(originVal){
+  const dt =  new Date(originVal)
+  const y =  dt.getFullYear()
+  const m = (dt.getMonth() + 1 +'').padStart(2,'0')
+  const d = (dt.getDate() + '').padStart(2,'0')
+  const hh = (dt.getHours() + '').padStart(2,'0')
+  const mm = (dt.getMinutes() + '').padStart(2,'0')
+  const ss = (dt.getSeconds() + '').padStart(2,'0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
+// 全局注册 vue-table-with-tree-grid
+Vue.component('tree-table',TreeTable)
+
+new Vue({
+  render: h => h(App),
+  router
+}).$mount('#app')
