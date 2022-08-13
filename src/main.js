05324da855dd854171ce5fa7584405fcc9cefd32
LiuChang <lcc1875@163.com>
2022-08-13 16:36:58 +0800

    NProgress

--- src/main.js
+++ src/main.js
@@ -5,8 +5,15 @@ import router from '@/router/index'
 import './plugins/element'
 import 'font-awesome/css/font-awesome.css'
 import TreeTable from 'vue-table-with-tree-grid'
+
+// 导入 NProgress包对应的JS和CSS
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 在request拦截器中展示进度条 NProgress.start()
+NProgress.start()
 // 设置请求拦截器，添加token令牌，保证拥有获取数据的权限
 // axios请求拦截 为每次API的请求挂载一个包含token的authorization字段的
 axios.interceptors.request.use(config=>{
@@ -15,6 +22,12 @@ axios.interceptors.request.use(config=>{
   // 在最后必须return config
   return config
 })
+// 在response拦截器中隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config=>{
+  NProgress.done()
+  return config
+})
+
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
